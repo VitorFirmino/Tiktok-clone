06860cbd447283720992f1ed8dfc55c053b9ed61
VitorFirmino <itachi@DESKTOP-DKN5CSR.localdomain>
2021-04-08 00:29:36 -0300

    :package: feat: Pages

---
+++ src/pages/index.js
@@ -0,0 +1,29 @@
+import Layout from '../components/Layout';
+import Feed from '../components/Feed';
+import Suggestions from '../components/Suggestions';
+import { openDB } from '../database/connection';
+
+
+function Index({ posts }) {
+  return (
+    <Layout>
+      <Feed posts={posts}></Feed>
+      <Suggestions></Suggestions>
+    </Layout>
+  );
+}
+
+export async function getServerSideProps(context) {
+
+  const db = await openDB();
+  const data = await db.collection('feed').find().toArray();
+
+
+  return {
+    props: {
+      posts: JSON.parse(JSON.stringify(data)),
+    },
+  };
+}
+
+export default Index;
